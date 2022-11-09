8c359d375aa6248e8f50c0073ca6a414d9fc697b
Kyouhei Hayama <kyouhei@cloudflare.com>
2022-11-09 23:28:57 +0900

    improvement

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,19 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+// pages/api/hello.js
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+// Next.js Edge API Routes: https://nextjs.org/docs/api-routes/edge-api-routes
+
+export const config = {
+  runtime: 'experimental-edge',
+}
+
+export default async function (req) {
+  return new Response(
+    JSON.stringify({ name: 'John Doe' }),
+    {
+      status: 200,
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+  )
 }
